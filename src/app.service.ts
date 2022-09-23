404f17bbb8b32a47498953909a0506004e1f4e47
hvlopez <hectorvmlopez@gmail.com>
2022-09-23 17:51:16 -0500

    adding tasks scheduling

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,11 +1,15 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { EventEmitter2, OnEvent } from '@nestjs/event-emitter';
+import { Cron, CronExpression, SchedulerRegistry } from '@nestjs/schedule';
 import { CreateUserRequest } from './dto/create-user.request';
 import { UserCreatedEvent } from './events/user-created.event';
 
 @Injectable()
 export class AppService {
-  constructor(private readonly eventEmitter: EventEmitter2) {}
+  constructor(
+    private readonly eventEmitter: EventEmitter2,
+    private schedule: SchedulerRegistry,
+  ) {}
   private readonly logger = new Logger(AppService.name);
   getHello(): string {
     return 'Hello World!';
@@ -17,6 +21,20 @@ export class AppService {
       'user.created',
       new UserCreatedEvent(userId, body.email),
     );
+
+    //establish a websocket connect to the user
+    const establishWebsocketTimeout = setTimeout(
+      () => this.establishWebsocketConnection(userId),
+      5000,
+    );
+
+    this.schedule.addTimeout(
+      `${userId}_establish_ws`,
+      establishWebsocketTimeout,
+    );
+  }
+  private establishWebsocketConnection(userId: string) {
+    this.logger.log('establishWebsocketConnection', userId);
   }
 
   @OnEvent('user.created')
@@ -30,5 +48,10 @@ export class AppService {
     this.logger.log('sendWelcomeGift', event.email);
     await new Promise<void>((resolve) => setTimeout(resolve, 3000));
     this.logger.log('this is your welcome gift', event.email);
+  }
+
+  @Cron(CronExpression.EVERY_10_SECONDS, { name: 'deleteExpiredUsers' })
+  deleteExpiredUsers() {
+    this.logger.log('deleteExpiredUsers');
   }
 }
